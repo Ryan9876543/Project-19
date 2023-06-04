953d5661380f5d9f1203cc1d04fc063728d93f3a
Ryan9876543 <128496613+Ryan9876543@users.noreply.github.com>
2023-06-04 11:24:05 +0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,15 @@
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+var score
+
 var towerImg, tower;
 var doorImg, doorsGroup;
 var climberImg, climber, climbersGroup;
 var ghost, ghostImg;
 var invisibleBlockGroup, invisibleBlock;
-var gameState = PLAY
-var gameState = END
+var gameOverImg, restartImg
+
 
 function preload(){
   towerImg = loadImage("tower.png");
@@ -12,6 +17,8 @@ function preload(){
   climberImg = loadImage("climber.png");
   ghostImg = loadImage("ghost-standing.png");
   spookySound = loadSound("spooky.wav");
+  restartImg = loadImage("restart.png")
+  gameOverImg = loadImage("gameOver.png")
 
   doorsGroup = new Group()
   climbersGroup = new Group()
@@ -22,17 +29,54 @@ function setup() {
   tower = createSprite(300,300);
   tower.addImage("tower",towerImg);
   tower.velocityY = 1;
+
   ghost = createSprite(200,200)
   ghost.addImage("ghost",ghostImg)
   ghost.scale = 0.35
 
+  gameOver = createSprite(300,100);
+  gameOver.addImage(gameOverImg);
   
+  restart = createSprite(300,300);
+  restart.addImage(restartImg);
+
+  gameOver.scale = 0.5;
+  restart.scale = 0.5;
+
+  score = 0
+
 }
+
+function reset(){
+  score = 0
+  gameState = PLAY
+  gameOver.visible = false
+  restart.visible = false
+  doorsGroup.destroyEach()
+  climbersGroup.destroyEach()
+  ghost = createSprite(200,200)
+  ghost.addImage("ghost",ghostImg)
+  ghost.scale = 0.35
+
+}
+
 
 function draw() {
   background(200);
+  text ("Score: "+ score, 500,50);
   
-  if(tower.y > 400){
+   score.depth = ghost.depth;
+   score.depth = score.depth + 11;
+
+
+  if(gameState === PLAY){ 
+    gameOver.visible = false
+    restart.visible = false
+
+  
+    score = score + Math.round(getFrameRate()/50); 
+
+    if(tower.y > 400){
       tower.y = 300
     }
 
@@ -49,10 +93,31 @@ function draw() {
       ghost.velocityY = -5
     }
 
-    if(ghost.x > 600){
-      gameState = END
+  spawnClimbers()
+  spawnDoors()
+
+  if(climbersGroup.isTouching(ghost) || ghost.x > 600){
+    gameState = END;  
+  }
+
+  }
+
+  else if (gameState === END) {
+     gameOver.visible = true;
+     restart.visible = true;
+    
+    
+     //set lifetime of the game objects so that they are never destroyed
+    climbersGroup.setLifetimeEach(-1);
+    doorsGroup.setLifetimeEach(-1);
+    
+
+    if (mousePressedOver(restart)) {
+     reset()
     }
 
+  }
+ 
 
     ghost.velocityY = ghost.velocityY + 0.8
     
